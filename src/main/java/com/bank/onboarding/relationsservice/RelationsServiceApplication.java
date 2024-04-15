347fcc6f36c9f72f9cd8b4494061b26f0ec43358
JoaoBruno1220256 <1220256@isep.ipp.pt>
2024-04-15 20:23:45 +0100

    Added MCS relations-service

---
+++ src/main/java/com/bank/onboarding/relationsservice/RelationsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bank.onboarding.relationsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/onboarding/relationsservice/RelationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.onboarding.relationsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
